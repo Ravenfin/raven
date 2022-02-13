22a295d5422a9dca6026ba531f31e87d92f56b22
Ohad Shivek <oshivek@outbrain.com>
2022-02-13 17:55:53 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { IndexManagementComponent } from './components/index-management/index-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    IndexManagementComponent
   ],
   imports: [
     BrowserModule,
